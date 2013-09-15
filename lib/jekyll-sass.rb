3cdf9f350376479c94445465e04bffd6301e1dc1
Zach Starkebaum <zach@relevantwalk.com>
2013-09-14 18:19:52 -0600

    Update so that when any sass file changes everything is rebuilt.
    Because there is often pretty deep linking in sass, rebuilding only one changed sass file doesn't update dependent files. Also, files with underscores are ignored by jekyll, its important for sass to rebuild when these files are modified.

--- lib/jekyll-sass.rb
+++ lib/jekyll-sass.rb
@@ -26,6 +26,9 @@ module Jekyll
     end
 
     class SassCssFile < StaticFile
+      class << self
+        attr_accessor :should_write_sass
+      end
 
       # Obtain destination path.
       #   +dest+ is the String path to the destination dir
@@ -44,9 +47,10 @@ module Jekyll
       #
       # Returns false if the file was not modified since last time (no-op).
       def write(dest)
-        dest_path = destination(dest)
-        return false if File.exist? dest_path and !modified?
+        return false if !SassCssFile.should_write_sass
+
         @@mtimes[path] = mtime
+        dest_path = destination(dest)
         FileUtils.mkdir_p(File.dirname(dest_path))
         begin
           content = File.read(path)
@@ -70,7 +74,12 @@ module Jekyll
         end
         true
       end
+    end
 
+    class SuppressedSassFile < StaticFile
+      def update_mtime
+        @@mtimes[path] = mtime
+      end
     end
 
     class SassCssGenerator < Generator
@@ -86,7 +95,85 @@ module Jekyll
             site.static_files.delete(sf)
             name = File.basename(sf.path)
             destination = File.dirname(sf.path).sub(site.source, '')
-            site.static_files << SassCssFile.new(site, site.source, destination, name)
+            sass_file =  SassCssFile.new(site, site.source, destination, name)
+            if sass_file.modified?
+              SassCssFile.should_write_sass = true
+            end
+            site.static_files << sass_file
+          end
+        end
+        supp_files = suppressed_sass_files(site)
+        if suppressed_files_modified?(supp_files)
+          SassCssFile.should_write_sass = true
+          update_files_mtime(supp_files)
+        end
+      end
+
+      def suppressed_files_modified?(files)
+        files.each do |file|
+          return true if file.modified?
+        end
+        return false
+      end
+
+      def update_files_mtime(files)
+        files.each do |file|
+          file.update_mtime
+        end
+      end
+
+      def suppressed_sass_files(site)
+        sass_matcher = /^_.*\.#{SassConfig.syntax(site)}/
+        suppressed_sass_paths = recursively_search_directories(site, sass_matcher)
+        files = [ ]
+        suppressed_sass_paths.each do |path|
+          name = File.basename(path)
+          destination = File.dirname(path).sub(site.source, '')
+          files << SuppressedSassFile.new(site, site.source, destination, name)
+        end
+        return files
+      end
+
+      # Recursively find files in site that match a regexp
+      # Does not ignore underscored files
+      # This could probably be shortened but
+      # we need to search like jekyll searches
+      #
+      # site - The Jekyll site
+      # matcher - a regexp for the filename's to find
+      # dir - the relative directory to recurse in
+      #
+      # Returns an Array of absolute paths that match
+      def recursively_search_directories(site, matcher, dir = '')
+        base = File.join(site.source, dir)
+        entries = Dir.chdir(base) { filter_files(Dir.entries('.'), site) }
+        found = [ ]
+        entries.each do |f|
+          f_abs = File.join(base, f)
+          found << f_abs if matcher.match(f)
+          if File.directory?(f_abs)
+            f_rel = File.join(dir, f)
+            if site.dest.sub(/\/$/, '') != f_abs
+              more_found = recursively_search_directories(site, matcher, f_rel)
+              found.concat(more_found)
+            end
+          end
+        end
+        return found
+      end
+
+      # Filter out any files/directories that are specifically
+      # excluded in the site config
+      #
+      # entries - The Array of String file/directory entries to filter.
+      #
+      # Returns the Array of filtered entries.
+      def filter_files(entries, site)
+        entries.reject do |e|
+          unless site.include.glob_include?(e)
+            ['.', '#'].include?(e[0..0]) ||
+            site.exclude.glob_include?(e) ||
+            (File.symlink?(e) && site.safe)
           end
         end
       end
